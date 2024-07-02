6ccd3ba4420026f0a8d079c313eec666a95c3c84
abrehan2 <rehannajam2@gmail.com>
2024-07-03 01:24:55 +0500

    Chore: Reduced a masonry column

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const Page = () => {
     <Loader />
   ) : (
     <main className="w-full max-w-full">
-      <Masonry columns={{ 640: 1, 768: 2, 1024: 3, 1280: 5 }} gap={16}>
+      <Masonry columns={{ 640: 1, 768: 2, 1024: 3, 1280: 3 }} gap={16}>
         {data?.posts.map((post: postCardProps) => (
           <PostCard
             key={post.title}
